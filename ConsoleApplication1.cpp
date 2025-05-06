34dbdaaab1909d947f530dc47bfbbabec751270f
Alexei Petrov <petrovAlekse95@mail.ru>
2025-05-06 17:49:48 +0300

    06 05 24

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -2,34 +2,37 @@
 #include <vector>
 
 template<typename T>
-void qrt(T& a) {
-    a = a * a;
+T qrt(T a) {
+    return a * a;
 }
 
 template<typename T>
-void qrt(std::vector<T>& a) {
+std::vector<T> qrt(std::vector<T> a) {
+
+    std::vector<T> b(a);
     for (int i{ 0 }; i < a.size(); i++) {
-          a[i] = a[i] * a[i];
+        b[i] = b[i] * b[i];
     }
+    return b;
 }
 
 
 int main()
 {
-    int data = 4;
-    std::cout << "[IN]: " << data << std::endl;
-    qrt(data);
+    int originalData = 4;
+    std::cout << "[IN]: " << originalData << std::endl;
+    int data = qrt(originalData);
     std::cout << "[OUT]: " << data << std::endl;
 
 
-    std::vector<int> dataVector = { -1, 4, 8 };
+    std::vector<int> originalDataVector = { -1, 4, 8 };
     std::cout << "[IN]: ";
-    for (int i{ 0 }; i < dataVector.size(); i++) {
-        std::cout << dataVector[i] << " ";
+    for (int i{ 0 }; i < originalDataVector.size(); i++) {
+        std::cout << originalDataVector[i] << " ";
     };
     std::cout << std::endl;
 
-    qrt(dataVector);
+    std::vector<int>dataVector = qrt(originalDataVector);
     std::cout << "[OUT]: ";
     for (int i{ 0 }; i < dataVector.size(); i++) {
         std::cout << dataVector[i] << " ";
